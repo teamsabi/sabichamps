02a26e7662ae94f08ebbc2d384edf7de9df6431f
teamsabi <sabiteam23@gmail.com>
2024-12-07 17:57:59 +0700

    update sweet alert jadwal guru

--- guru/layout/top.php
+++ guru/layout/top.php
@@ -28,6 +28,7 @@ $username = $_SESSION['username'];
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
     <!-- Bootstrap 5 -->
     <link href="../../css/bootstrap.min.css">
--- guru/page/jadwal/proses.php
+++ guru/page/jadwal/proses.php
@@ -19,41 +19,43 @@ if (isset($_POST['aksi'])) {
                   VALUES ('$hari', '$tanggal', '$tempat', '$namaKelas', '$mapel', '$jamMulai', '$jamSelesai', '$namaGuru')";
 
         $sql = mysqli_query($conn, $query);
+        $status = 'tambah';
 
         if($sql){
-            header("location: Jadwal.php");
+            header("location: Jadwal.php?status=sukses&aksi=$status");
         }else{
-            echo $query;
+            header("location: Jadwal.php?status=gagal&aksi=$status");
         }
         }else if($_POST['aksi'] == "edit"){
-        // echo "edit data <a href='Jadwal.php'>[Home]</a>";
+            $id_jadwal = $_POST['id_jadwal'];
+            $hari = $_POST['hari'];
+            $tanggal = $_POST['tanggal'];
+            $tempat = $_POST['tempat'];
+            $namaKelas = $_POST['nama_kelas'];
+            $mapel = $_POST['mapel'];
+            $jamMulai = $_POST['jam_mulai'];
+            $jamSelesai = $_POST['jam_selesai'];
+            $namaGuru = $_POST['nama_guru'];
 
-        $id_jadwal = $_POST['id_jadwal'];
-        $hari = $_POST['hari'];
-        $tanggal = $_POST['tanggal'];
-        $tempat = $_POST['tempat'];
-        $namaKelas = $_POST['nama_kelas'];
-        $mapel = $_POST['mapel'];
-        $jamMulai = $_POST['jam_mulai'];
-        $jamSelesai = $_POST['jam_selesai'];
-        $namaGuru = $_POST['nama_guru'];
+            $query = "UPDATE jadwal SET hari = '$hari', tanggal = '$tanggal', tempat = '$tempat', nama_kelas = '$namaKelas',
+            mapel = '$mapel', jam_mulai = '$jamMulai', jam_selesai = '$jamSelesai', nama_guru = '$namaGuru'  WHERE id_jadwal = '$id_jadwal';";
 
-        $query = "UPDATE jadwal SET hari = '$hari', tanggal = '$tanggal', tempat = '$tempat', nama_kelas = '$namaKelas',
-        mapel = '$mapel', jam_mulai = '$jamMulai', jam_selesai = '$jamSelesai', nama_guru = '$namaGuru'  WHERE id_jadwal = '$id_jadwal';";
-        $sql = mysqli_query($conn, $query); 
-        header("location: Jadwal.php");
+            $status = 'edit';
+
+            $sql = mysqli_query($conn, $query); 
+            header("location: Jadwal.php?status=sukses&aksi=$status");
         }
-        }
+}
 
         if (isset($_GET['hapus'])) {   
-            $id_jadwal = $_GET['hapus'];  // Mengambil ID dari URL
-            
-            // Query untuk menghapus data
+            $id_jadwal = $_GET['hapus'];
+
             $query = "DELETE FROM jadwal WHERE id_jadwal = '$id_jadwal';";
             $sql = mysqli_query($conn, $query);
+            $status = 'hapus';
         
             if ($sql) {
-                header("location: Jadwal.php");
+                header("location: Jadwal.php?status=sukses&aksi=$status");
             } else {
                 echo $query;
             }
