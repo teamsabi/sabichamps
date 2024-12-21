05d85ba4c7da9fe83e4882e4c103d3a8e31eb49a
teamsabi <sabiteam23@gmail.com>
2024-12-21 13:11:13 +0700

    Update landing page

--- landing-page.php
+++ landing-page.php
@@ -205,73 +205,6 @@
           persiapan ujian nasional dan tes masuk perguruan tinggi dengan lebih percaya diri.</p>
       </div><!-- End Section Title -->
 
-    <!-- Gallery Section -->
-    <section id="gallery" class="gallery section">
-
-      <!-- Section Title -->
-      <div class="container section-title" data-aos="fade-up">
-        <h2>Aplikasi Mobile</h2>
-        <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
-      </div><!-- End Section Title -->
-
-      <div class="container-fluid" data-aos="fade-up" data-aos-delay="100">
-
-        <div class="swiper init-swiper">
-          <script type="application/json" class="swiper-config">
-            {
-              "loop": true,
-              "speed": 600,
-              "autoplay": {
-                "delay": 5000
-              },
-              "slidesPerView": "auto",
-              "centeredSlides": true,
-              "pagination": {
-                "el": ".swiper-pagination",
-                "type": "bullets",
-                "clickable": true
-              },
-              "breakpoints": {
-                "320": {
-                  "slidesPerView": 1,
-                  "spaceBetween": 0
-                },
-                "768": {
-                  "slidesPerView": 3,
-                  "spaceBetween": 30
-                },
-                "992": {
-                  "slidesPerView": 5,
-                  "spaceBetween": 30
-                },
-                "1200": {
-                  "slidesPerView": 7,
-                  "spaceBetween": 30
-                }
-              }
-            }
-          </script>
-          <div class="swiper-wrapper align-items-center">
-            <div class="swiper-slide"><a class="glightbox" data-gallery="images-gallery-full" href="assets2/img/app-gallery/app-gallery-1.png"><img src="assets2/img/app-gallery/app-gallery-1.png" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="glightbox" data-gallery="images-gallery-full" href="assets2/img/app-gallery/app-gallery-2.png"><img src="assets2/img/app-gallery/app-gallery-2.png" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="glightbox" data-gallery="images-gallery-full" href="assets2/img/app-gallery/app-gallery-3.png"><img src="assets2/img/app-gallery/app-gallery-3.png" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="glightbox" data-gallery="images-gallery-full" href="assets2/img/app-gallery/app-gallery-4.png"><img src="assets2/img/app-gallery/app-gallery-4.png" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="glightbox" data-gallery="images-gallery-full" href="assets2/img/app-gallery/app-gallery-5.png"><img src="assets2/img/app-gallery/app-gallery-5.png" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="glightbox" data-gallery="images-gallery-full" href="assets2/img/app-gallery/app-gallery-6.png"><img src="assets2/img/app-gallery/app-gallery-6.png" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="glightbox" data-gallery="images-gallery-full" href="assets2/img/app-gallery/app-gallery-7.png"><img src="assets2/img/app-gallery/app-gallery-7.png" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="glightbox" data-gallery="images-gallery-full" href="assets2/img/app-gallery/app-gallery-8.png"><img src="assets2/img/app-gallery/app-gallery-8.png" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="glightbox" data-gallery="images-gallery-full" href="assets2/img/app-gallery/app-gallery-9.png"><img src="assets2/img/app-gallery/app-gallery-9.png" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="glightbox" data-gallery="images-gallery-full" href="assets2/img/app-gallery/app-gallery-10.png"><img src="assets2/img/app-gallery/app-gallery-10.png" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="glightbox" data-gallery="images-gallery-full" href="assets2/img/app-gallery/app-gallery-11.png"><img src="assets2/img/app-gallery/app-gallery-11.png" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a class="glightbox" data-gallery="images-gallery-full" href="assets2/img/app-gallery/app-gallery-12.png"><img src="assets2/img/app-gallery/app-gallery-12.png" class="img-fluid" alt=""></a></div>
-          </div>
-          <div class="swiper-pagination"></div>
-        </div>
-
-      </div>
-
-    </section><!-- /Gallery Section -->
-
     <section id="faq" class="faq" style="background-color: #ffffff;">
       <div class="container" data-aos="fade-up">
   
@@ -350,15 +283,10 @@
               <div class="testimonial-item">
                 <img src="assets2/img/testimonials/Syaiful.png" class="testimonial-img" alt="">
                 <h3>Syaiful Amin</h3>
-                <h4>Suka Audyaa &amp; Website Desaigner</h4>
+                <h4>Project Manager &amp; Website Desaigner</h4>
                 <div class="stars">
                   <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
                 </div>
-                <p>
-                  <i class="bi bi-quote quote-icon-left"></i>
-                  <span>Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.</span>
-                  <i class="bi bi-quote quote-icon-right"></i>
-                </p>
               </div>
             </div><!-- End testimonial item -->
 
@@ -366,15 +294,10 @@
               <div class="testimonial-item">
                 <img src="assets2/img/testimonials/putra.png" class="testimonial-img" alt="">
                 <h3>Dwi Rasyari Putra</h3>
-                <h4>Pro Fanny &amp; Tukang Ngocok</h4>
+                <h4>Developer Mobile</h4>
                 <div class="stars">
                   <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
                 </div>
-                <p>
-                  <i class="bi bi-quote quote-icon-left"></i>
-                  <span>Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.</span>
-                  <i class="bi bi-quote quote-icon-right"></i>
-                </p>
               </div>
             </div><!-- End testimonial item -->
 
@@ -382,15 +305,10 @@
               <div class="testimonial-item">
                 <img src="assets2/img/testimonials/Karin.png" class="testimonial-img" alt="">
                 <h3>Kariena Adelia Maharani</h3>
-                <h4>Store Owner</h4>
+                <h4>UI/UX Designer</h4>
                 <div class="stars">
                   <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
                 </div>
-                <p>
-                  <i class="bi bi-quote quote-icon-left"></i>
-                  <span>Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.</span>
-                  <i class="bi bi-quote quote-icon-right"></i>
-                </p>
               </div>
             </div><!-- End testimonial item -->
 
@@ -398,15 +316,10 @@
               <div class="testimonial-item">
                 <img src="assets2/img/testimonials/enggar.png" class="testimonial-img" alt="">
                 <h3>Nadhifatus Aulia E</h3>
-                <h4>Freelancer</h4>
+                <h4>Developer Web</h4>
                 <div class="stars">
                   <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
                 </div>
-                <p>
-                  <i class="bi bi-quote quote-icon-left"></i>
-                  <span>Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.</span>
-                  <i class="bi bi-quote quote-icon-right"></i>
-                </p>
               </div>
             </div><!-- End testimonial item -->
 
@@ -414,15 +327,10 @@
               <div class="testimonial-item">
                 <img src="assets2/img/testimonials/Dicky.png" class="testimonial-img" alt="">
                 <h3>Dicky Aries Setiawan</h3>
-                <h4>Pro Melisa &amp; Cagur </h4>
+                <h4>Developer Mobile</h4>
                 <div class="stars">
                   <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
                 </div>
-                <p>
-                  <i class="bi bi-quote quote-icon-left"></i>
-                  <span> aku ingin terbang tinggi menembus awan</span>
-                  <i class="bi bi-quote quote-icon-right"></i>
-                </p>
               </div>
             </div><!-- End testimonial item -->
 
@@ -495,24 +403,15 @@
   <footer id="footer" class="footer dark-background">
     <div class="container">
       <h3 class="sitename">SABI</h3>
-      <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
-      <div class="social-links d-flex justify-content-center">
-        <a href=""><i class="bi bi-twitter-x"></i></a>
-        <a href=""><i class="bi bi-facebook"></i></a>
-        <a href=""><i class="bi bi-instagram"></i></a>
-        <a href=""><i class="bi bi-skype"></i></a>
-        <a href=""><i class="bi bi-linkedin"></i></a>
-      </div>
       <div class="container">
         <div class="copyright">
-          <span>Copyright</span> <strong class="px-1 sitename">SABI</strong> <span>Politeknik Negeri Jember-JTI</span>
+          <span>Copyright</span> <strong class="px-1 sitename">Team SABI</strong> <span>Politeknik Negeri Jember-JTI</span>
         </div>
         <div class="credits">
           <!-- All the links in the footer should remain intact. -->
           <!-- You can delete the links only if you've purchased the pro version. -->
           <!-- Licensing information: https://bootstrapmade.com/license/ -->
           <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-          Designed by <a href="https://bootstrapmade.com/">TEAM SABI</a>
         </div>
       </div>
     </div>
