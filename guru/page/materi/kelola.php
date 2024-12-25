11ef66bef0bb36a268738872ca5d15a89a0c635a
teamsabi <sabiteam23@gmail.com>
2024-12-26 03:24:46 +0700

    Update Page Materi

--- guru/page/materi/kelola.php
+++ guru/page/materi/kelola.php
@@ -1,22 +1,34 @@
 <?php
 require_once '../../layout/top.php';
-require_once '../../helper/conek.php';
+require_once '../../helper/config.php';
 
-$kode_materi = '';
+$id_materi = '';
 $judul_materi = '';
-$mapel = '';
-$nama_kelas = '';
-$nama_guru = '';
-$kelasjadwal = mysqli_query($conn, "SELECT nama_kelas FROM kelas");
-$gurukelas = mysqli_query($conn, "SELECT nama_guru FROM guru");
-$mata_pelajaran = mysqli_query($conn, "SELECT nama_mapel FROM mapel");
+$nama_mapel = '';
+$nama_lengkap = '';
 
 // Mengecek apakah ada parameter 'ubah' di URL
 if (isset($_GET['ubah'])) {
-    $kode_materi = $_GET['ubah'];
+    $id_materi = $_GET['ubah'];
 
     // Query untuk mengambil data berdasarkan ID materi
-    $query = "SELECT * FROM materi WHERE kode_materi = '$kode_materi'";
+    $query = "
+        SELECT 
+            materi.id_materi, 
+            materi.judul_materi, 
+            mapel.nama_mapel, 
+            user.nama_lengkap
+        FROM 
+            materi
+        JOIN 
+            mapel ON materi.kode_mapel = mapel.kode_mapel
+        JOIN 
+            materi_siswa ON materi.id_materi = materi_siswa.id_materi
+        JOIN 
+            user ON materi_siswa.id_user = user.id_user
+        WHERE 
+            materi.id_materi = '$id_materi';
+    ";
     $sql = mysqli_query($conn, $query);
     
     // Cek apakah data ditemukan
@@ -24,11 +36,10 @@ if (isset($_GET['ubah'])) {
         $result = mysqli_fetch_assoc($sql);
         
         // Ambil data dari database
-        $kode_materi = $result['kode_materi'];
+        $id_materi = $result['id_materi'];
         $judul_materi = $result['judul_materi'];
-        $mapel = $result['mapel'];
-        $nama_kelas = $result['nama_kelas'];
-        $nama_guru = $result['nama_guru'];
+        $nama_mapel = $result['nama_mapel'];
+        $nama_lengkap = $result['nama_lengkap'];
     } else {
         echo "Data tidak ditemukan!";
         exit;
@@ -44,61 +55,60 @@ if (isset($_GET['ubah'])) {
                 <div class="col-md-11">
                     <div class="card">
                         <div class="card-header mb-4">
-                            <h4 class="card-title">Tambah Materi</h4>
+                            <h4 class="card-title"><?php echo isset($_GET['ubah']) ? 'Ubah Materi' : 'Tambah Materi'; ?></h4>
                         </div>
                         <div class="card-body">
-                            <input type="hidden" value="<?php echo $kode_materi; ?>" name="kode_materi">
+                            <input type="hidden" value="<?php echo $id_materi; ?>" name="kode_materi">
+                            
+                            <!-- Input untuk Judul Materi -->
                             <div class="form-group row">
                                 <label for="judulmateri" class="col-sm-3 col-form-label">Judul Materi</label>
                                 <div class="col-sm-9">
                                     <input required="text" name="judul_materi" class="form-control" id="judulmateri" placeholder="Namakan Judul Materi" value="<?php echo $judul_materi;?>">
                                 </div>
                             </div>
+
+                            <!-- Dropdown untuk Mata Pelajaran -->
                             <div class="form-group row">
                                 <label for="mapel" class="col-sm-3 col-form-label">Mata Pelajaran</label>
                                 <div class="col-sm-9">
-                                  <select class="form-control" name="mapel" id="mapel" required>
+                                    <select class="form-control" name="mapel" id="mapel" required>
                                         <option value="">--Pilih Mata Pelajaran--</option>
                                         <?php
-                                        while ($r = mysqli_fetch_array($mata_pelajaran)) :
+                                        // Query untuk mengambil daftar mata pelajaran
+                                        $mapel_query = "SELECT kode_mapel, nama_mapel FROM mapel";
+                                        $mapel_result = mysqli_query($conn, $mapel_query);
+                                        while ($r = mysqli_fetch_array($mapel_result)) :
                                         ?>
-                                        <option value="<?= $r['nama_mapel'] ?>"><?= $r['nama_mapel'] ?></option>
-                                        <?php
-                                        endwhile;
-                                        ?>
-                                    </select>  
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="kelasmateri" class="col-sm-3 col-form-label">Kelas</label>
-                                <div class="col-sm-9">
-                                    <select class="form-control" name="nama_kelas" id="kelasmateri" required>
-                                        <option value="">--Pilih Kelas--</option>
-                                        <?php
-                                        while ($r = mysqli_fetch_array($kelasjadwal)) :
-                                        ?>
-                                        <option value="<?= $r['nama_kelas'] ?>"><?= $r['nama_kelas'] ?></option>
+                                        <option value="<?= $r['kode_mapel'] ?>" <?= ($r['nama_mapel'] == $nama_mapel) ? 'selected' : ''; ?>><?= $r['nama_mapel'] ?></option>
                                         <?php
                                         endwhile;
                                         ?>
                                     </select>
                                 </div>
                             </div>
+
+                            <!-- Input untuk File Materi -->
                             <div class="form-group row">
                                 <label for="filemateri" class="col-sm-3 col-form-label">File Materi</label>
                                 <div class="col-sm-9">
                                     <input <?php if(!isset($_GET['ubah'])){ echo "required";} ?> type="file" name="file_materi" class="form-control-file" id="filemateri">
                                 </div>
                             </div>
+
+                            <!-- Dropdown untuk Nama Guru -->
                             <div class="form-group row">
                                 <label for="gurumateri" class="col-sm-3 col-form-label">Nama Guru</label>
                                 <div class="col-sm-9">
-                                    <select class="form-control" name="nama_guru" id="gurumateri" required>
+                                    <select class="form-control" name="nama_lengkap" id="gurumateri" required>
                                         <option value="">--Pilih Guru Pengajar--</option>
                                         <?php
-                                        while ($r = mysqli_fetch_array($gurukelas)) :
+                                        // Query untuk mengambil daftar guru
+                                        $guru_query = "SELECT * FROM user WHERE role = 'guru'";
+                                        $guru_result = mysqli_query($conn, $guru_query);
+                                        while ($r = mysqli_fetch_array($guru_result)) :
                                         ?>
-                                        <option value="<?= $r['nama_guru'] ?>"><?= $r['nama_guru'] ?></option>
+                                        <option value="<?= $r['id_user'] ?>" <?= ($r['nama_lengkap'] == $nama_lengkap) ? 'selected' : ''; ?>><?= $r['nama_lengkap'] ?></option>
                                         <?php
                                         endwhile;
                                         ?>
